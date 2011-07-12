908116c8780448fa67215db9ad4a2ea4c1dd5177
Freek Wielstra <fr.wielstra@gmail.com>
2011-07-12 12:09:49 +0200

    Added fraction demo (and made compiling work).

---
+++ fraction/fraction.m
@@ -0,0 +1,24 @@
+#import "fraction.h"
+#import <stdio.h>
+
+@implementation Fraction
+-(void) print {
+  printf("%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator: (int) n {
+    numerator = n;
+}
+
+-(void) setDenominator: (int) d {
+    denominator = d;
+}
+
+-(int) denominator {
+    return denominator;
+}
+
+-(int) numerator {
+    return numerator;
+}
+@end
